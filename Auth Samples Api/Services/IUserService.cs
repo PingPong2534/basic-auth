363478abed07b823c9c37916242e3c7afc4dd737
Ping <news4ping@gmail.com>
2023-08-05 20:12:32 +0700

    create

---
+++ Auth Samples Api/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using BasicAuth.Models;
+
+namespace BasicAuth.Services
+{
+    public interface IUserService
+    {
+        Task<User> AuthenticateAsync(string username, string password);
+    }
+}
---
+++ Auth Samples Api/Services/UserService.cs
@@ -0,0 +1,21 @@
+﻿using BasicAuth.Models;
+
+namespace BasicAuth.Services
+{
+    public class UserService : IUserService
+    {
+        Task<User> IUserService.AuthenticateAsync(string username, string password)
+        {
+            User user = null;
+
+            if("test".Equals(username) && "test123".Equals(password))
+                user = new User()
+                {
+                    Id = "1",
+                    Username = username
+                };
+
+            return Task.FromResult(user);
+        }
+    }
+}
